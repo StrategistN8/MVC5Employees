3f6aec58fa0ccc150fa9b25c0a35e45c64dcf0a3
jimlyons <the_stratigest@hotmail.com>
2020-02-05 01:00:51 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Employees.Models;
+
+namespace MVC5Employees.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Main Menu
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // About Page
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        // Contact Page
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        // Form Confirm
+        [HttpPost]
+        public ActionResult AddEmp (int empID, string empName, int empSal)
+        {
+            Employee e = new Employee()
+            {
+                Id = empID,
+                Name = empName,
+                Salary = empSal
+            };
+          
+            return View("EmpConfirm", e);
+        }
+
+        // New Employee Form
+        public ActionResult AddEmp()
+        {
+            return View("NewEmpForm");
+        }
+
+        // Dynamic Page Action Method
+        public ActionResult GrossPayCalculator()
+        {
+            return View();
+        }
+    
+        // View with Arrays and Looping
+        public ActionResult MainOfficeEmployees()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Employees.Models
+{
+    public class Employee
+    {
+
+        #region PROPERTIES
+        public int Id { get; set; }
+        public int Salary { get; set; }
+        public string Name { get; set; }
+        #endregion
+
+        #region CONSTRUCTORS
+        
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public Employee()
+        {
+
+        }
+
+        #endregion
+    }
+}
